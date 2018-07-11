01f03a1a0764e23020216c9a41eb3d3d0186031f
TenzenIga <arhan-93@list.ru>
2018-07-12 00:02:15 +0300

    working on header tab

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import {Image, Glyphicon} from 'react-bootstrap';
  const Sidebar = ()=>{
 
   return(
     <div class="sidebar">
-  <a href="#">Dashboard</a>
+    <div className='sidebar-avatar'>
+      <Image src="https://pp.userapi.com/c636426/v636426984/1227a/2VSA_Wc-voo.jpg" circle />
+      <div className="sidebar-controllers">
+        <ul >
+            <li ><a><Glyphicon glyph="align-left"/></a></li>
+            <li><a><Glyphicon glyph="comment"/></a></li>
+            <li><a><Glyphicon glyph="stats"/></a></li>
+            <li><a><Glyphicon glyph="tasks"/></a></li>
+            <li className="active"><a><Glyphicon glyph="bell"/></a></li>
+          </ul>
+        </div>
+  </div>
+
+    <div className="sidebar-menu">
+    <h3 className="sidebar-header">Pages</h3>
+<ul >
+ <a href="#">Dashboard</a>
   <a href="#">Mailbox</a>
   <a href="#">Gallery</a>
   <a href="#">Social</a>
@@ -30,6 +46,8 @@ import React from 'react';
     <a href='#'>Lock Page</a>
     <a href='#'>Invoice</a>
     <a href='#'>Pricing Tables</a>
+    </ul>
+    </div>
 </div>
 
   );
